041e93a310c409bb1c0339de4aaa6ad41019bf60
Abhinand <abhin4nd6@gmail.com>
2024-11-11 16:59:07 +0530

    fix: fix in server

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,15 +16,31 @@ app.use(express.urlencoded({
     extended: true
 }));
 const morganStream = {
-    write: (message: any) => logger.info(message.trim()) 
+    write: (message: any) => logger.info(message.trim())
 };
 app.use(morgan('combined', { stream: morganStream }));
 
 app.use(cookie())
 app.use(cors(corsOption))
 app.use(helmet({
-    crossOriginResourcePolicy:{policy:'same-origin'}
-}))
+    crossOriginResourcePolicy: {
+        policy: 'cross-origin',
+    },
+    contentSecurityPolicy: {
+        directives: {
+            defaultSrc: ["'self'"], // Restricts everything to same origin by default
+            scriptSrc: ["'self'", 'https://article-mern.vercel.app'], // Allow scripts from the frontend
+            styleSrc: ["'self'", 'https://fonts.googleapis.com'], // Example for allowing Google Fonts
+            imgSrc: ["'self'", 'data:', 'https://article-mern.vercel.app'], // Allow images from frontend
+            connectSrc: ["'self'", 'https://article-mern.vercel.app'], // Allow API requests from frontend
+            fontSrc: ["'self'", 'https://fonts.gstatic.com'], // Allow fonts from specific sources
+            objectSrc: ["'none'"], // Disallow <object>, <embed>, <applet> for security
+            frameAncestors: ["'self'"], // Prevent embedding in frames or iframes on other sites
+            upgradeInsecureRequests: [], // Automatically upgrade HTTP requests to HTTPS
+        },
+    }
+},
+))
 app.use('/api', router)
 
 
